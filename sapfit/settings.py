54181f868c7924968a725564eb6cce781d03b8cb
whyme <ritikshrestha94@gmail.com>
2026-05-22 10:42:36 +0545

    some style change

--- sapfit/settings.py
+++ sapfit/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
