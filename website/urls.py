4ff2187a9b7e4884578f0b8f790b68f980f9f86c
whyme <ritikshrestha94@gmail.com>
2026-05-27 15:55:36 +0545

    some changes in database

--- website/urls.py
+++ website/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from website.views import client_view, delete_client, index, BlogPostView, BlogDetailView, PostView, service_detail_view, edit_client
+from website.views import client_view, delete_client, index, BlogPostView, BlogDetailView, service_detail_view, edit_client
 
 urlpatterns = [
     path('', index, name ="home-page"),
     path('services/<slug:slug>/', service_detail_view, name ="service-detail"),
     path('blogs/', BlogPostView, name='blogs'),
-    path('posts/', PostView, name='posts'),
     path('blogs/<slug:slug>/', BlogDetailView, name='blog-detail'),
     path('clients/', client_view, name='client-view'),
     path('delete/<int:id>', delete_client, name='delete-client'),
