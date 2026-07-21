97d0c5a1dbb11018458b18cf47baafeec4d3ac62
Ritik Lal Shrestha <77373533+whyme-duh@users.noreply.github.com>
2026-07-21 21:42:19 +0545

    Change STATIC_ROOT to staticfiles_build directory

--- sapfit/settings.py
+++ sapfit/settings.py
@@ -139,7 +139,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'staticfiles')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
