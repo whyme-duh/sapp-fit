69109acfb65a70eb1f70c3b47dec4c875555f111
whyme <ritikshrestha94@gmail.com>
2026-07-21 20:54:29 +0545

    changed the DB with a connection string

--- sapfit/settings.py
+++ sapfit/settings.py
@@ -75,14 +75,18 @@ WSGI_APPLICATION = 'sapfit.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("DB_NAME"),         
-        'USER': os.environ.get("DB_USER"), 
-        'PASSWORD': os.environ.get("DB_PASSWORD"),   
-        'HOST': os.environ.get("DB_HOST"),            
-        'PORT': os.environ.get("DB_PORT"),                
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.environ.get("DB_NAME"),         
+    #     'USER': os.environ.get("DB_USER"), 
+    #     'PASSWORD': os.environ.get("DB_PASSWORD"),   
+    #     'HOST': os.environ.get("DB_HOST"),            
+    #     'PORT': os.environ.get("DB_PORT"),                
+    # }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 
