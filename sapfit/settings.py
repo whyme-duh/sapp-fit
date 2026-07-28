f67b368b14e174de7e706be22463d0da0b9d5164
whyme <ritikshrestha94@gmail.com>
2026-07-28 22:30:52 +0545

    Changed the email format to more stylistic with HTML embedded.

--- sapfit/settings.py
+++ sapfit/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY_SETTINGS')
 GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # CUSTOM VARIABLE FOR TURNING ON EMAILS
 
